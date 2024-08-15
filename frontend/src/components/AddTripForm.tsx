808f42685291495abd93bfb87cd788f4a795f865
Son Pham <trung.son.pham@draphony.com>
2024-08-15 11:19:53 +0700

    init and done basic CRUD

---
+++ frontend/src/components/AddTripForm.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import './AddTripForm.css';
+import { useCollection } from '@squidcloud/react';
+import { Trip } from '../types';
+
+function AddTrip() {
+  const [country, setCountry] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [countries, setCountries] = useState([]);
+  const tripsCollection = useCollection<Trip>('trips');
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then((res) => {
+        if (res.ok) return res.json();
+      })
+      .then((data) => {
+        const countryNames = data.map((country: any) => country.name.common);
+        setCountries(countryNames);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const addTrip = () => {
+    const tripId = crypto.randomUUID();
+    tripsCollection.doc(tripId).insert({
+      id: tripId,
+      country,
+      startDate: new Date(startDate),
+      endDate: new Date(endDate),
+      notes: [],
+    });
+  };
+  return (
+    <div className="trip-container">
+      <h3>Add Trip</h3>
+      <div className="trip-form">
+        <select value={country} onChange={(e) => setCountry(e.target.value)}>
+          <option value="" disabled>
+            {' '}
+            Select Country
+          </option>
+          {countries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </select>
+        <input
+          type="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+        <input
+          type="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+        <button onClick={addTrip}>Add Trip</button>
+      </div>
+    </div>
+  );
+}
+
+export default AddTrip;
---
+++ frontend/src/components/AskAI.tsx
@@ -0,0 +1,45 @@
+import { useSquid } from '@squidcloud/react';
+import React, { useState } from 'react';
+import './AskAI.css';
+import LoadingIndicator from './LoadingIndicator';
+const AskAI = () => {
+  const [text, setText] = useState('');
+  const [result, setResult] = useState('');
+  const [loading, setLoading] = useState(false);
+  const squid = useSquid();
+
+  const askPressed = async () => {
+    if (!text) return;
+    setLoading(true);
+    const result = await squid.executeFunction('askQuestion', text);
+    setResult(result);
+    setText('');
+    setLoading(false);
+  };
+  const closeResult = () => {
+    setResult('');
+  };
+  return (
+    <div className="container">
+      <h3>Ask a Question</h3>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      {loading ? (
+        <LoadingIndicator />
+      ) : (
+        <button onClick={askPressed}>Ask</button>
+      )}
+      {result && (
+        <div className="result-container">
+          \<textarea name="" value={result} rows={4}></textarea>
+          <button onClick={closeResult} className="close-button"></button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AskAI;
